214509147370d8455f8571945a5b4e84c14cefbc
sofiagabriele <sofia_hein@estudante.sesisenai.org.br>
2025-08-26 15:15:07 -0300

    1- Cálculo mf inicial
    Esta etapa é o cálculo de média inicial.

--- src/AppMediaFinal.java
+++ src/AppMediaFinal.java
@@ -1,5 +1,14 @@
 public class AppMediaFinal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       float n1, n2, n3, nf;
+
+       n1 = 7.8f;
+       n2 = 5.5f;
+       n3 = 4.9f;
+
+       nf = (n1+n2+n3)/3;
+
+       System.out.println("NF: "+nf);
+    
     }
 }
